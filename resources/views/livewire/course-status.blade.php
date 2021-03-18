5c7c604da180eeb221a2146ffefcf9fd7d042e51
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-03-18 09:31:21 +0100

    working on course status view

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -1,35 +1,88 @@
 <div class="mt-8">
     <div class="container grid grid-cols-3 gap-8">
         <div class="col-span-2">
-            {!!$current->iframe!!}
-            {{$current->name}}
+            <div class="embed-responsive">
+                {!!$current->iframe!!}
+            </div>
+            
+            <h1 class="text-3xl text-gray-600 font-bold mt-4">
+                {{$current->name}}
+            </h1>
+
+            @if ($current->description)
+                <div class="text-gray-600">
+                    {{$current->description->name}}
+                </div>
+            @endif
+
+            <div class="flex items-center mt-4 cursor-pointer">
+                <i class="fas fa-toggle-off text-2xl text-gray-600"></i>
+                <p class="text-sm ml-2">
+                    Marcar lección como completada
+                </p>
+            </div>
+
+            <div class="card mt-2">
+                <div class="card-body flex text-gray-500 font-bold">
+                    @if ($this->previous)
+                        <a wire:click="changeLesson({{$this->previous}})" class="cursor-pointer">Lección anterior</a>
+                    @endif
+                    @if ($this->next)
+                        <a wire:click="changeLesson({{$this->next}})" class="ml-auto cursor-pointer">Lección siguiente</a>
+                    @endif
+                </div>
+            </div>
+
         </div>
 
         <div class="card">
             <div class="card-body">
-                <h1>
+                <h1 class="text-2xl leading-8 text-center mb-4">
                     {{$course->title}}
                 </h1>
                 <div class="flex items-center">
                     <figure>
-                        <img src="{{ $course->teacher->profile_photo_url }}" alt="{{ $course->teacher->name }}">
+                        <img class="w-12 h-12 object-cover rounded-full mr-4" src="{{ $course->teacher->profile_photo_url }}" alt="{{ $course->teacher->name }}">
                     </figure>
                     <p>
                         {{ $course->teacher->name }}
-                        <a class="text-blue-500">{{'@'.Str::slug($course->teacher->name, '')}}</a>
+                        <a class="text-blue-500 text-sm">{{'@'.Str::slug($course->teacher->name, '')}}</a>
                     </p>
                 </div>
 
+                <p class="text-gray-600 text-sm mt-2">
+                    20% completado
+                </p>
+
+                <div class="relative pt-1">
+                    <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-200">
+                      <div style="width:30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+                    </div>
+                  </div>
+
                 <ul>
                     @foreach ($course->sections as $section)
-                        <li>
-                            <a class="font-bold">{{ $section->name }}</a>
+                        <li class="text-gray-600 mb-4">
+                            <a class="font-bold text-base inline-block mb-2">{{ $section->name }}</a>
                             <ul>
                                 @foreach ($section->lessons as $lesson)
-                                    <li>
-                                        <a class="cursor-pointer" wire:click="changeLesson({{$lesson}})">{{$lesson->name}} @if ($lesson->completed)
-                                            (Completado)
-                                        @endif</a>
+                                    <li class="flex">
+                                        <div>
+                                            @if ($lesson->completed)
+                                                @if ($lesson->id == $current->id)
+                                                    <span class="inline-block w-4 h-4 border-2 border-yellow-300 rounded-full mr-2 mt-1"></span> 
+                                                @else
+                                                    <span class="inline-block w-4 h-4 bg-yellow-300 rounded-full mr-2 mt-1"></span> 
+                                                @endif
+                                            @else
+                                            @if ($lesson->id == $current->id)
+                                            <span class="inline-block w-4 h-4 border-2 border-gray-500 rounded-full mr-2 mt-1"></span>
+                                                @else
+                                                    <span class="inline-block w-4 h-4 bg-gray-500 rounded-full mr-2 mt-1"></span>
+                                                @endif
+                                            @endif
+                                        </div>
+                                        <a class="cursor-pointer" wire:click="changeLesson({{$lesson}})">{{$lesson->name}}</a>
                                     </li>
                                 @endforeach
                             </ul>
