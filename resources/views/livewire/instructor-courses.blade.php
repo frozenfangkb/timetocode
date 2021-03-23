10612507d3af4566e4b1449590884f4664435e1d
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-03-23 09:12:21 +0100

    Instructor course view

--- resources/views/livewire/instructor-courses.blade.php
+++ resources/views/livewire/instructor-courses.blade.php
@@ -1,3 +1,121 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="container py-8">
+    <x-table-responsive>
+        <div class="px-6 py-4">
+            <input wire:keydown="reset_page" wire:model="search" class="form-input w-full shadow-sm h-10 p-2 rounded-sm" placeholder="Buscar curso">
+        </div>
+        @if ($courses->count())
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Nombre
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Alumnos
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Calificación
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Estado
+                        </th>
+                        <th scope="col" class="relative px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($courses as $course)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 h-10 w-10">
+                                        @if (strpos($course->image->url, "picsum"))
+                                            <img class="w-10 h-10 rounded-full" src="{{$course->image->url}}" alt="" />
+                                        @else
+                                            <img class="w-10 h-10 rounded-full" src="{{Storage::url($course->image->url)}}" alt="">
+                                        @endif
+                                    </div>
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            {{$course->title}}
+                                        </div>
+                                        <div class="text-sm text-gray-500">
+                                            {{$course->category->name}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$course->students_count}}</div>
+                                <div class="text-sm text-gray-500">Alumnos matriculados</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900 flex items-center">
+                                    {{$course->rating}}
+                                    <ul class="flex text-sm ml-2">
+                                        <li class="mr-1">
+                                            <i class="fas fa-star text-{{$course->rating >= 1 ? 'yellow' : 'gray'}}-400"></i>
+                                        </li>
+                                        <li class="mr-1">
+                                            <i class="fas fa-star text-{{$course->rating >= 2 ? 'yellow' : 'gray'}}-400"></i>
+                                        </li>
+                                        <li class="mr-1">
+                                            <i class="fas fa-star text-{{$course->rating >= 3 ? 'yellow' : 'gray'}}-400"></i>
+                                        </li>
+                                        <li class="mr-1">
+                                            <i class="fas fa-star text-{{$course->rating >= 4 ? 'yellow' : 'gray'}}-400"></i>
+                                        </li>
+                                        <li class="mr-1">
+                                            <i class="fas fa-star text-{{$course->rating == 5 ? 'yellow' : 'gray'}}-400"></i>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="text-sm text-gray-500">Valoración</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                @switch($course->status)
+                                    @case(1)
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                            Borrador
+                                        </span>
+                                        @break
+                                    @case(2)
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                            En revisión
+                                        </span>
+                                        @break
+                                    @case(3)
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Publicado
+                                        </span>
+                                        @break
+                                    @default
+                                        
+                                @endswitch
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    <!-- More items... -->
+                </tbody>
+            </table>
+            <div class="px-6 py-4">
+                {{ $courses->links() }}
+            </div>
+        @else
+            <div class="px-6 py-4">
+                No encontramos cursos con ese filtro...
+            </div>
+        @endif
+    </x-table-responsive>
 </div>
